b41b4eb072bc550c1d3e5ed0e263594596a7261b
lhq <3289468135@qq.com>
2024-07-27 23:04:29 +0800

    06

--- springMVC3/src/main/java/controller/loginController.java
+++ springMVC3/src/main/java/controller/loginController.java
@@ -91,9 +91,6 @@ public class loginController {
     public String login8(@RequestParam("hello") String userName,HttpServletRequest request) {
         request.getSession().setAttribute("user",userName);
         System.out.println("userName="+userName);
-        System.out.println("hhhhh");
-        System.out.println("xxxxx");
-        System.out.println("hello");
         return "redirect:login0";
     }
 
